b595b3345ba0f04d93eca36dcba383d41cb8cad8
wilvanson <wilvanson.dutervil@students.cookman.edu>
2022-04-09 17:47:40 -0400

    questions

---
+++ Interview Question #3.py
@@ -0,0 +1,12 @@
+
+# Ask if i can just make this number a int 
+# num = int(input('Enter a number: '))
+
+num = float(input('Enter a number: '))
+
+if num == 0:
+    print("Zero")
+elif num >= 0:
+    print('Positive number')
+else:
+    print('Negative number')
