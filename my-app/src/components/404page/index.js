3b02bab524cbe9382fe63ba5a794fe03719c6545
wilvanson <wilvanson.dutervil@students.cookman.edu>
2022-04-10 19:10:56 -0400

    download pakages

---
+++ my-app/src/components/404page/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const CrashPage = () => {
+
+    return (
+        <div className='lost'>
+        <h1>The Page you are looking for is not found</h1>
+        <img src='https://media.istockphoto.com/photos/error-page-not-found-picture-id1153372686?b=1&k=20&m=1153372686&s=170667a&w=0&h=4veHHEjI5rq2qpYWMXcHSzXfrp22SMwa79DsqqiYfAw='/>
+        </div>
+    )
+};
+  
+export default CrashPage;
