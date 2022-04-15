a183b064931e17e6f09751f97638648296082d52
wilvanson <wilvanson.dutervil@students.cookman.edu>
2022-04-15 18:25:02 -0400

    fixes

--- app/__init__.py
+++ app/__init__.py
@@ -10,3 +10,10 @@ app.register_blueprint(data_routes, url_prefix='/api/data')
 
 # Application Security
 CORS(app)
+
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>')
+def react_root(path):
+    if path == 'favicon.ico':
+        return app.send_static_file('favicon.ico')
+    return app.send_static_file('index.html')
